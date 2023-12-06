444d284473bf7497f0999058592aa3825c9f628d
pari3266 <parisatalebi25@gmail.com>
2023-12-06 10:52:53 +0330

    popular dishes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { animation } from '@angular/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(),
     importProvidersFrom([
-      
+      // animation()
     ])
   ],
 };
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NewRestaurantsListComponent } from "./new-restaurants-list/new-restaurants-list.component";
 import { TasteOfPeopleComponent } from "./taste-of-people/taste-of-people.component";
+import { PopularDishesComponent } from "./popular-dishes/popular-dishes.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [NewRestaurantsListComponent, TasteOfPeopleComponent]
+    imports: [NewRestaurantsListComponent, TasteOfPeopleComponent, PopularDishesComponent]
 })
 export class HomeComponent {
 
